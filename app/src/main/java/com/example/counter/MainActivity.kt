25a28ea78d50f3b4a633f6256257fb2442db6ea2
Uriel Aispuro <urielalexis64@gmail.com>
2021-08-28 15:29:56 -0600

    add onClick and onLongClick events, persistent data

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.counter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var count: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setCount()
+
+        binding.btnSum.setOnClickListener {
+            count++
+            setCount()
+        }
+
+        binding.btnSum.setOnLongClickListener {
+            count = 0
+            setCount()
+            true
+        }
+    }
+
+    private fun setCount() {
+        binding.tvCount.text = count.toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(PARAM_COUNT, count)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        count = savedInstanceState.getInt(PARAM_COUNT)
+        setCount()
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    companion object {
+        private val PARAM_COUNT = "param_count"
     }
 }
